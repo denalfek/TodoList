ff709830832d68e7d31b7b3bc46b8d3a13a75967
Denis Sokolov <denalfek@gmail.com>
2020-08-16 12:38:26 +0300

    make component controlled

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -16,18 +16,21 @@ export default class AddItemForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
     };
 
     render() {
         return (
             <form className="add-item-form d-flex"
                   onSubmit={this.onSubmit}>
-                {this.state.label}
 
                 <input type="text"
                        className="form-control"
                        onChange={this.onLableChange}
-                       placeholder="set target" />
+                       placeholder="set target"
+                       value={this.state.label} />
 
                 <button
                     className="btn btn-outline-secondary">
