f620151d5405ff02c839fcc00d97840222e2b9d9
Sokolov Denis <denalfek@gmail.com>
2024-02-11 13:59:22 +0300

    new project; fix build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function TodoItem() {
+  return (
+    <span>Drink Coffee</span>
+  );
+}
+
+ReactDOM.render(<TodoItem />, document.getElementById('root'));
