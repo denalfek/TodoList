5034732e9ad4f5b3283f007b9b28728841c196ce
Sokolov Denis <denalfek@gmail.com>
2024-02-24 15:12:21 +0300

    without bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel'
+import TodoList from './components/todo-list';
 
-function TodoItem() {
+const App = () => {
+
+  const todoData = [
+    {id:1, label: 'Drink Coffee', important: false},
+    {id:2, label: 'Create React App', important: true},
+    {id:3, label: 'Have A Lunch', important: false},
+    {id:4, label: 'Watch A Movie', important: true},
+  ];
+
   return (
-    <span>Drink Coffee</span>
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos = {todoData} />
+    </div>
   );
 }
 
-ReactDOM.render(<TodoItem />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />, );
